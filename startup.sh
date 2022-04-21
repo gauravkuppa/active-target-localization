b8ef408cdb5eae7a69b9f9241be41b24803f5967
Gaurav Kuppa <gauravkuppa@client0360vag.vpn.private.upenn.edu>
2022-04-20 22:17:53 -0400

    edit Dockerfile

---
+++ startup.sh
@@ -0,0 +1,12 @@
+conda create -n atl_venv python=3.6
+conda activate atl_venv
+conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
+pip install visdom gym matplotlib
+#pip install -r requirements.txt
+#pip install -r rllib_requirements.txt
+#pip install ray[rllib]
+
+mkdir /home/code
+cd /home/code
+
+git clone https://github.com/gauravkuppa/active-target-localization.git
