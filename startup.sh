9a3366ce5fcf37c0b51fca8eb66f5f0cc2ec9063
Gaurav Kuppa <gauravkuppa@huntsman-ve703-04612.apn.wlan.private.upenn.edu>
2022-04-21 07:35:23 -0400

    updated startup script

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 conda create -n atl_venv python=3.6
 conda activate atl_venv
 conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
-pip install visdom gym matplotlib tqdm
+pip install visdom==0.1.8 gym==0.17.2 matplotlib tqdm
 #pip install -r requirements.txt
 #pip install -r rllib_requirements.txt
 #pip install ray[rllib]
